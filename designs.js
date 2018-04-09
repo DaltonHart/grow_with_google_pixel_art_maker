7dbcd5cdcbfba4231fb3e2f2677755c3fb8cfd8a
Dalton <daltonvaren@gmail.com>
2018-04-09 16:35:02 -0700

    added logic to submit form to capture data and apply to values height and width

--- designs.js
+++ designs.js
@@ -1,10 +1,21 @@
 // Select color input
 // Select size input
-
+var height, width, color;
 // When size is submitted by the user, call makeGrid()
+$('#sizePicker').submit(function(event){
+    event.preventDefault();
+    height = $("#inputHeight").val();
+    width = $("#inputWidth").val();
+    console.log(height);
+    console.log(width);
+    makeGrid(height, width);
+});
 
-function makeGrid() {
 
-// Your code goes here!
-
+function makeGrid(h, y) {
+    //reset grid
+    $('tr').remove();
+    for (var i = 1; i<= h; i++){
+        $('#pixelCanvas').append('<tr id=table' + i + '></tr>')
+    }
 }
